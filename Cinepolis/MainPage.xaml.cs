f31b7d07e8851e251b66f9a68ed5a7c9a2ae185f
Alejandra <estefany.crug@gmail.com>
2023-08-06 15:12:37 -0600

    Agregar archivos de proyecto.

---
+++ Cinepolis/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Cinepolis.ModelViews;
+using Cinepolis.Views;
+using System;
+using Xamarin.Forms;
+
+namespace Cinepolis.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+            NavigationPage.SetHasBackButton(this, false);
+            BindingContext = this;
+        }
+
+        private async void OnSignInButtonClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new SignInPage());
+        }
+
+        private async void OnSignUpButtonClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new SignUpPage());
+        }
+
+        private async void CarteleraButtonClicked(object sender, EventArgs e)
+        {
+
+            InvitadoStatus.Value = true;
+            await Navigation.PushAsync(new Location());
+
+        }
+    }
+}
---
+++ Cinepolis/Views/UserProfilePage.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Cinepolis.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class UserProfilePage : ContentPage
+    {
+
+        public string Nombre { get; set; }
+        public string DNI { get; set; }
+        public string Correo { get; set; }
+        public string Telefono { get; set; }
+        public string Direccion { get; set; }
+
+        public UserProfilePage()
+        {
+            InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, true);
+
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            await LoadPerfil();
+            BindingContext = this;
+        }
+
+        public async Task LoadPerfil()
+        {
+            try
+            {
+                using (HttpClient httpClient = new HttpClient())
+                {
+                    //httpClient.DefaultRequestHeaders.Add("Authorization", "Token " + Application.Current.Properties["token"]);
+                    httpClient.DefaultRequestHeaders.Add("Authorization", "Token c79975fcb5ee8d521f52daa3fceb9b3008f67ede");
+                    HttpResponseMessage response = await httpClient.GetAsync("http://64.227.10.233/auth/cliente");
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string jsonResponse = await response.Content.ReadAsStringAsync();
+                        var data = JsonConvert.DeserializeObject<dynamic>(jsonResponse);
+                        Nombre = data?.data?.cliente?.first_name + " " + data?.data?.cliente?.last_name;
+                        DNI = data?.data?.cliente?.DNI;
+                        Correo = data?.data?.cliente?.email;
+                        Telefono = data?.data?.cliente?.telefono;
+                        Direccion = data?.data?.cliente?.direccion;
+                    }
+                    else
+                    {
+                        await DisplayAlert("Error", "Failed to fetch perfil from the API", "OK");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Error", "An error occurred while fetching cities from the API", "OK");
+            }
+        }
+
+
+    }
+}
